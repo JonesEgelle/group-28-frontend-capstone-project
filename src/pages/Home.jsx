6899ec443b34dc50d0c20c2f61d56dd80718f456
ngyoukenneth <ngyoukenneth@gmail.com>
2026-06-06 20:28:44 +0100

    update facts section/ Home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Hero from '../components/Hero/Hero';
-import PlanetSection from '../components/PlanetSection/PlanetSection';
-import VideoSection from '../components/videoSection/VideoSection';
-import FactsSection from '../components/FactsSection/FactsSection';
-import ContactForm from '../components/ContactForm/ContactForm';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import PlanetSection from "../components/PlanetSection/PlanetSection";
+import VideoSection from "../components/videoSection/VideoSection";
+import FactsSection from "../components/FactsSection/FactsSection";
+import ContactForm from "../components/ContactForm/ContactForm";
+import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
-      <main className='main-content'>
+      <main className="main-content">
         {/* Hero Section */}
         <Hero />
 
