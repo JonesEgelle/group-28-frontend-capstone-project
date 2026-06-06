9198da35bf2a799de743d0f83db73fa825594d3d
IamNumbrd0010 <abdulsalamjamiu10@gmail.com>
2026-06-06 15:55:01 +0100

    Update video

--- src/components/videoSection/VideoSection.jsx
+++ src/components/videoSection/VideoSection.jsx
@@ -41,7 +41,7 @@ const VideoSection = () => {
               src='https://videotourl.com/videos/1775122754836-288be386-cff3-4e87-abe9-4d5ca47a81ba.mp4'
               type='video/mp4'
             />
-            Your browser doesn't support this video tag.
+            Your browser does not support the video tag.
           </video>
         </div>
 
@@ -51,7 +51,7 @@ const VideoSection = () => {
           <p>
             Planetary science goes beyond images. Comparing <span>mass</span>,{' '}
             <span>diameter</span>, <span>gravity</span>, and{' '}
-            <span>density</span>, we can gain insight into how planets form, behave,
+            <span>density</span>, we gain insight into how planets form, behave,
             and interact within the solar system.
           </p>
         </div>
