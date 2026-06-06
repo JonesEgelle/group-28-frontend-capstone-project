902292d0e1dcd8505944362ab3c80bde0b5e114c
markytech01 <markytech01@gmail.com>
2026-06-06 21:00:26 +0100

    updated videosection

--- src/components/videoSection/VideoSection.jsx
+++ src/components/videoSection/VideoSection.jsx
@@ -52,7 +52,7 @@ const VideoSection = () => {
           <p>
             Planetary science goes beyond images. Comparing <span>mass</span>,{' '}
             <span>diameter</span>, <span>gravity</span>, and{' '}
-            <span>density</span>, we gain insight into how planets form, behave,
+            <span>density</span>, we gain insight into how planets form, behave
             and interact within the solar system.
           </p>
         </div>
